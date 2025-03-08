b1f601923965321e62f26971fdbf23305b4c5542
dissmaestro <masimka1302@yanedx.ru>
2025-03-08 15:11:13 +0300

    add create dish

--- backend/internal/handlers/crud_dishes.go
+++ backend/internal/handlers/crud_dishes.go
@@ -21,10 +21,28 @@ func SelectDishById(queries *db.Queries) fiber.Handler {
 
 		res, err := queries.GetDishByID(c.Context(), int32(id))
 		if err != nil {
-			log.Println("Error with SelectAgent: ", err)
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to get agent"})
+			log.Println("Error with SelectDish: ", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to get dish"})
 		}
 
 		return c.JSON(res)
+	}
+}
+
+func createDishes(queries *db.Queries) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var params db.InsertDishParams
+		if err := c.BodyParser(&params); err != nil {
+			log.Println("Error parsing request body:", err)
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
+		}
+
+		created, err := queries.InsertDish(c.Context(), params)
+		if err != nil {
+			log.Println("Error with Create Dish: ", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to create dishes"})
+		}
+
+		return c.Status(fiber.StatusCreated).JSON(created)
 	}
 }
